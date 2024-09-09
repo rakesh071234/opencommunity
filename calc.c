350eb828b11d2303a87d8c81ee362c5cbe8dc7e8
rakesh071234 <113282283+rakesh071234@users.noreply.github.com>
2024-09-09 09:33:23 +0530

    sub functinality implemented - calc.c modified sub.c file added

--- calc.c
+++ calc.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
 int add(int, int);
+int sub(int, int);
 
 int main(){
 	int a, b;
 	printf("enter 2 numbers: ");
 	scanf("%d %d", &a, &b);
 	printf("add result:%d", add(a, b));
+	printf("\nsub result:%d", sub(a, b));
 	return 0;
 }
